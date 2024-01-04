290c6f51edfbb8dad886ce9fbf16d912028ffc11
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-03 22:32:50 -0800

    Cleaned up the javascript to reduce the numbers of lines needed for the game, changed parts of strings to use object calls, and added line breaks in the prompt and confirm alerts.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Janken
+
 A Rock-Paper-Scissors game in Javascript.
+
+Tutorials used:
+
+https://www.bitdegree.org/learn/best-code-editor/javascript-confirm-example-3 Line breaks
--- script.js
+++ script.js
@@ -3,35 +3,43 @@ var playerScore = 0;
 var computerScore = 0;
 var tie = 0;
 
+function playGame() {
 // give player prompt and choices
-var PlayerChoice = prompt("Let's play Rock Paper Scissors! Pick r for rock, p for paper, and s for scissors.");
+var PlayerChoice = prompt("Janken is the Japanese name for 'Rock Paper Scissors.'\nLet's play Janken!\nPick r for rock, p for paper, and s for scissors.");
 
+if (prompt === false) {
+    text = "Maybe Next time!";
+} else {
+    playGame();
+}
 
 // computer chooses R, P, or S.
 var choices = ["r", "p", "s"];
 
 var computerChoice = choices [Math.floor (Math.random() * choices.length)];
 
-// Game play
-if (PlayerChoice === "r" && computerChoice === "s") {
-    playerScore++;
-    confirm("You chose rock and the Computer chose scissors. You Win! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
-} else if (PlayerChoice === "s" && computerChoice === "p") {
-    playerScore++;
-    confirm("You chose scissors and the Computer chose paper. You Win! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
-} else if (PlayerChoice === "p" && computerChoice === "r") {
-    playerScore++;
-    confirm("You chose paper and the Computer chose rock. You Win! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
-} else if (PlayerChoice === "r" && computerChoice === "p") {
-    computerScore++;
-    confirm("You chose rock and the Computer chose paper. You Lose! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
-} else if (PlayerChoice === "s" && computerChoice === "r") {
-    computerScore++;
-    confirm("You chose scissors and the Computer chose rock. You Lose! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
-} else if (PlayerChoice === "p" && computerChoice === "s") {
-    
-    computerScore++;confirm("You chose Paper and the Computer chose scissors. You Lose! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
-} else {
+// Game play, score tally, and asking if player wants to play again
+if (PlayerChoice === computerChoice) {
     tie++;
-    confirm("It was a tie! Player: " + playerScore + " Computer: " + computerScore + " Ties " + tie + " Do you want to play again?.");
+    confirm("You chose " + PlayerChoice +" and the Computer chose " + computerChoice + ". It was a tie!\nPlayer: " + playerScore + "\nComputer: " + computerScore + "\nTies " + tie + "\nDo you want to play again?.");
+    tie++;
+} else if (
+(PlayerChoice === "r" && computerChoice === "s") ||
+(PlayerChoice === "s" && computerChoice === "p") ||
+(PlayerChoice === "p" && computerChoice === "r") ) {
+    playerScore++;
+    confirm("You chose " + PlayerChoice +" and the Computer chose " + computerChoice + ". You Win!\nPlayer: " + playerScore + "\nComputer: " + computerScore + "\nTies " + tie + "\nDo you want to play again?.");
+} else {
+    computerScore++;
+    confirm("You chose " + PlayerChoice +" and the Computer chose " + computerChoice + ". You Lose!\nPlayer: " + playerScore + "\nComputer: " + computerScore + "\nTies " + tie + "\nDo you want to play again?.");
 }
+
+if (confirm == false) {
+    text = "Thanks for Playing!";
+} else {
+    playGame();
+}
+
+};
+
+playGame();
