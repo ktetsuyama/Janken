fb4bd41c0c3e34a632c4efe0b7f9a9ab9d0790c2
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-04 22:01:34 -0800

    Fixed the problem of the game not looping, or letting me quit. Added the 'Thanks for Playing' message on quit

--- script.js
+++ script.js
@@ -4,42 +4,78 @@ var computerScore = 0;
 var tie = 0;
 
 function playGame() {
-// give player prompt and choices
-var PlayerChoice = prompt("Janken is the Japanese name for 'Rock Paper Scissors.'\nLet's play Janken!\nPick r for rock, p for paper, and s for scissors.");
+  // give player prompt and choices
+  var PlayerChoice = prompt(
+    "Janken is the Japanese name for 'Rock Paper Scissors.'\nLet's play Janken!\nPick r for rock, p for paper, and s for scissors."
+  );
 
-if (prompt === false) {
-    text = "Maybe Next time!";
-} else {
-    playGame();
-}
+  // computer chooses R, P, or S.
+  var choices = ["r", "p", "s"];
 
-// computer chooses R, P, or S.
-var choices = ["r", "p", "s"];
+  var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-var computerChoice = choices [Math.floor (Math.random() * choices.length)];
+  var playAgain = false;
 
-// Game play, score tally, and asking if player wants to play again
-if (PlayerChoice === computerChoice) {
+  // Game play, score tally, and asking if player wants to play again
+  if (PlayerChoice === computerChoice) {
     tie++;
-    confirm("You chose " + PlayerChoice +" and the Computer chose " + computerChoice + ". It was a tie!\nPlayer: " + playerScore + "\nComputer: " + computerScore + "\nTies " + tie + "\nDo you want to play again?.");
+    playAgain = confirm(
+      "You chose " +
+        PlayerChoice +
+        " and the Computer chose " +
+        computerChoice +
+        ". It was a tie!\nPlayer: " +
+        playerScore +
+        "\nComputer: " +
+        computerScore +
+        "\nTies " +
+        tie +
+        "\nDo you want to play again?."
+    );
     tie++;
-} else if (
-(PlayerChoice === "r" && computerChoice === "s") ||
-(PlayerChoice === "s" && computerChoice === "p") ||
-(PlayerChoice === "p" && computerChoice === "r") ) {
+  } else if (
+    (PlayerChoice === "r" && computerChoice === "s") ||
+    (PlayerChoice === "s" && computerChoice === "p") ||
+    (PlayerChoice === "p" && computerChoice === "r")
+  ) {
     playerScore++;
-    confirm("You chose " + PlayerChoice +" and the Computer chose " + computerChoice + ". You Win!\nPlayer: " + playerScore + "\nComputer: " + computerScore + "\nTies " + tie + "\nDo you want to play again?.");
-} else {
+    playAgain = confirm(
+      "You chose " +
+        PlayerChoice +
+        " and the Computer chose " +
+        computerChoice +
+        ". You Win!\nPlayer: " +
+        playerScore +
+        "\nComputer: " +
+        computerScore +
+        "\nTies " +
+        tie +
+        "\nDo you want to play again?."
+    );
+  } else {
     computerScore++;
-    confirm("You chose " + PlayerChoice +" and the Computer chose " + computerChoice + ". You Lose!\nPlayer: " + playerScore + "\nComputer: " + computerScore + "\nTies " + tie + "\nDo you want to play again?.");
-}
+    playAgain = confirm(
+      "You chose " +
+        PlayerChoice +
+        " and the Computer chose " +
+        computerChoice +
+        ". You Lose!\nPlayer: " +
+        playerScore +
+        "\nComputer: " +
+        computerScore +
+        "\nTies " +
+        tie +
+        "\nDo you want to play again?."
+    );
+  }
 
-if (confirm == false) {
-    text = "Thanks for Playing!";
-} else {
+  // var playAgain = window.confirm();
+
+  if (playAgain === false) {
+    alert("Thanks for Playing!");
+  } else {
     playGame();
+  }
 }
-
-};
 
 playGame();
